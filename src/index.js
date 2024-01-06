f47df13d93884c287f3e52243a0823cd1389167a
Rahmathullaaiman <142215408+Rahmathullaaiman@users.noreply.github.com>
2024-01-06 18:02:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from './components/redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-//provider is used tostore  globally accessible
+
     <Provider store={store}>
        <App />
        </Provider>
